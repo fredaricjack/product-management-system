6fbe38bd6ca7b3f0e418a8dfad097647551c920c
fredaricjack <fredaricjack14@gmail.com>
2026-03-23 12:58:57 +0530

    Fix database creation on Render

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -38,4 +38,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.Run();
